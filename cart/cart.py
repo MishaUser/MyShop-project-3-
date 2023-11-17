fecee4cd94da24c2c0da01a0f59f02228d593bd6
misha <u-mi21@yandex.ru>
2023-11-17 18:17:47 +0300

    Добавление товаров в корзину/ исправление ошибок

--- cart/cart.py
+++ cart/cart.py
@@ -24,10 +24,10 @@ class Cart:
         """
         product_id = str(product.id)
         if product_id not in self.cart:
-            self.cart[product_id] = {'quality': 0,
+            self.cart[product_id] = {'quantity': 0,
                                      'price': str(product.price)}
         if override_quantity:
-            self.cart[product_id]['quality'] = quantity
+            self.cart[product_id]['quantity'] = quantity
         else:
             self.cart[product_id]['quantity'] += quantity
         self.save()
@@ -54,13 +54,13 @@ class Cart:
         """
         product_ids = self.cart.keys()
         # получить объекты product и добавить их в корзину
-        products = Product.object.filter(id__in=product_ids)
+        products = Product.objects.filter(id__in=product_ids)
         cart = self.cart.copy()
         for product in products:
             cart[str(product.id)]['product'] = product
         for item in cart.values():
             item['price'] = Decimal(item['price'])
-            item['total_price'] = item['price'] * item['quality']
+            item['total_price'] = item['price'] * item['quantity']
             yield item
 
     def __len__(self):
