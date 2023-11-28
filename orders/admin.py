57626d192a27b6f4587eb22ffa17fb2e06a003f0
misha <u-mi21@yandex.ru>
2023-11-28 18:06:35 +0300

    Расширение сайта администрирования

--- orders/admin.py
+++ orders/admin.py
@@ -5,6 +5,7 @@ from django.utils.safestring import mark_safe
 import csv
 import datetime
 from django.http import HttpResponse
+from django.urls import reverse
 
 
 def export_to_csv(modeladmin, request, queryset):
@@ -41,11 +42,16 @@ def order_stripe_payment(obj):
     return ''
 order_stripe_payment.short_description = 'Stripe payment'
 
+def order_detail(obj):
+    url = reverse('orders:admin_order_detail', args=[obj.id])
+    return mark_safe(f'<a href="{url}">Views</a>')
+
 @admin.register(Order)
 class OrderAdmin(admin.ModelAdmin):
     list_display = ['id', 'first_name', 'last_name', 'email',
                     'address', 'postal_code', 'city', 'paid',
-                    order_stripe_payment, 'created', 'updated']
+                    order_stripe_payment, 'created', 'updated',
+                    order_detail]
     list_filter = ['paid', 'created', 'updated']
     inlines = [OrderItemInline]
     actions = [export_to_csv]
